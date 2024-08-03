941b03f994d51361bf9213d7ff312a8e4bd1196f
mahmoudzanoun13 <mahmoudzanoun35@yahoo.com>
2024-08-03 11:43:57 +0300

    Fix style

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ export const ProjectCard = ({ title, description, imgUrl, github }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} alt="project pic" />
+        <img src={imgUrl} alt="project pic" style={{ height: "195px" }} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
